24fe7c43aa545a65aee5b3884d9ef16a7f69602b
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 19:42:39 +0530

    Incorrect data type usage in cin/cout

---
+++ 20.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter a number: ";
+    cin >> a; // Enter a string like "abc"
+    cout << "You entered: " << a;
+    return 0;
+}
